33b74ad7a6b2ac42188882b3201f796d4a8b009f
Aluno0326 <Aluno0326@univas.edu.br>
2016-05-31 22:12:02 -0300

    Atualizando...

--- js/ListController.js
+++ js/ListController.js
@@ -15,6 +15,31 @@ var ListController = {
         console.log(inputName.value);
         inputName.value = "";
         event.preventDefault();
+    },
+    
+    //TODO move it to HtmlService.js
+    addName: function(name) {
+        ListService.addName(name);
+        ListController.displayAddedName(name);
+    },
+    
+    displayAddedName: function(text) {
+        //TODO move it to HtmlService.js
+        var li = ListController.createListItem(text);
+        ListController.addToList(li);
+    },
+
+    //TODO move it to HtmlService.js
+    createListItem: function(text) {
+        var li = document.createElement('li');
+        li.innerHTML = text;
+        return li;
+    },
+
+    //TODO move it to HtmlService.js
+    addToList: function(listItem) {
+        var ul = document.getElementById('list-name');
+        ul.appendChild(li);
     }
     
 };
